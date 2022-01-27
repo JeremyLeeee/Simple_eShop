c2d87840b4d9631059e6766c8a10f10291b7a8ce
JeremyLeeee <determinehust@gmail.com>
2022-01-28 00:45:52 +0800

    add user model, repo, service

---
+++ repositories/user_repositories.go
@@ -0,0 +1,101 @@
+package repositories
+
+import (
+	"database/sql"
+	"errors"
+	"eshop/common"
+	"eshop/datamodels"
+	"strconv"
+)
+
+type IUser interface {
+	Conn() error
+	Select(userName string) (*datamodels.User, error)
+	Insert(user *datamodels.User) (int64, error)
+}
+
+type UserManager struct {
+	table     string
+	mysqlConn *sql.DB
+}
+
+func NewUserRepository(table string, db *sql.DB) IUser {
+	return &UserManager{table: table, mysqlConn: db}
+}
+
+func (u *UserManager) Conn() (err error) {
+	if u.mysqlConn == nil {
+		mysql, err := common.NewMysqlConn()
+		if err != nil {
+			return err
+		}
+		u.mysqlConn = mysql
+	}
+	if u.table == "" {
+		u.table = "order"
+	}
+	return
+}
+
+func (u *UserManager) Select(userName string) (user *datamodels.User, err error) {
+	if userName == "" {
+		return &datamodels.User{}, errors.New("user name is null")
+	}
+	if err = u.Conn(); err != nil {
+		return &datamodels.User{}, err
+	}
+
+	sql := "SELECT * FROM " + u.table + " WHERE userName=?"
+	rows, err := u.mysqlConn.Query(sql, userName)
+
+	if err != nil {
+		return &datamodels.User{}, err
+	}
+	result := common.GetResultRow(rows)
+
+	if len(result) == 0 {
+		return &datamodels.User{}, errors.New("user not exist")
+	}
+
+	user = &datamodels.User{}
+	common.DataToStructByTagSql(result, user)
+	return
+
+}
+
+func (u *UserManager) Insert(user *datamodels.User) (id int64, err error) {
+	if err = u.Conn(); err != nil {
+		return 0, err
+	}
+
+	sql := "INSERT " + u.table + " SET nickName=?, userName=?, passWord=?"
+	stmt, err := u.mysqlConn.Prepare(sql)
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := stmt.Exec(user.NickName, user.UserName, user.HashPassword)
+	if err != nil {
+		return 0, err
+	}
+
+	return result.LastInsertId()
+}
+
+func (u *UserManager) SelectById(id int64) (user *datamodels.User, err error) {
+	if err = u.Conn(); err != nil {
+		return &datamodels.User{}, err
+	}
+	sql := "SELECT * FROM " + u.table + " WHERE ID=" + strconv.FormatInt(id, 10)
+	rows, err := u.mysqlConn.Query(sql)
+	if err != nil {
+		return &datamodels.User{}, err
+	}
+	result := common.GetResultRow(rows)
+	if len(result) == 0 {
+		return &datamodels.User{}, errors.New("user not exist")
+	}
+	user = &datamodels.User{}
+	common.DataToStructByTagSql(result, user)
+	return
+}
---
+++ servies/user_service.go
@@ -0,0 +1,59 @@
+package servies
+
+import (
+	"eshop/datamodels"
+	"eshop/repositories"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type IUserService interface {
+	IsPwdSuccess(userName string, pwd string) (user *datamodels.User, isOk bool)
+	GetUserByName(string) (*datamodels.User, error)
+	AddUser(*datamodels.User) (int64, error)
+}
+
+type UserService struct {
+	userRepository repositories.IUser
+}
+
+func NewUserService(userRepository repositories.IUser) IUserService {
+	return &UserService{userRepository}
+}
+
+func (s *UserService) IsPwdSuccess(userName string, pwd string) (user *datamodels.User, isOk bool) {
+	user, err := s.userRepository.Select(userName)
+	if err != nil {
+		return &datamodels.User{}, false
+	}
+	isOK, _ := validatePassword(pwd, user.HashPassword)
+	if !isOK {
+		return &datamodels.User{}, false
+	}
+	return
+}
+
+func (s *UserService) GetUserByName(userName string) (user *datamodels.User, err error) {
+	return s.userRepository.Select(userName)
+}
+
+func (s *UserService) AddUser(user *datamodels.User) (ID int64, err error) {
+	pwdByte, err := generatePassword(user.HashPassword)
+	if err != nil {
+		return ID, err
+	}
+	user.HashPassword = string(pwdByte)
+	return s.userRepository.Insert(user)
+}
+
+func validatePassword(pwd string, hashedPwd string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPwd), []byte(pwd))
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func generatePassword(pwd string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+}
